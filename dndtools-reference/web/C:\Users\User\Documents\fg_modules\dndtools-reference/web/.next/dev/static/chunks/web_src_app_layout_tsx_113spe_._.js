24ea63ff9c02218e159719f373abf9c0c55221e7
root <lucasmendes.lx@gmail.com>
2026-09-14 23:34:01 -0300

    update

---
+++ dndtools-reference/web/C:\Users\User\Documents\fg_modules\dndtools-reference/web/.next/dev/static/chunks/web_src_app_layout_tsx_113spe_._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__1tg-m_z._.css","static/chunks/web_19vrbw2._.js"],
+    source: "dynamic"
+});
